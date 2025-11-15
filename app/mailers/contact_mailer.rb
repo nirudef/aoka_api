16c2b82d88a96be32776a16ab34904bac7713f54
nirudef <2372356@gmail.com>
2025-11-15 10:11:11 +0500

    мелкие правки

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,11 +1,11 @@
 # app/mailers/contact_mailer.rb
 class ContactMailer < ApplicationMailer
-  default to: '7657721@gmail.com'
+  # default to: '7657721@gmail.com'
 
   def notify_admin
     @name = params[:name]
     @message = params[:message]
     @lang = params[:lang]
-    mail(from: params[:email], subject: "Новое сообщение с сайта (#{@lang.upcase})")
+    mail(to: "7657721@gmail.com", reply_to: params[:email], subject: "Новое сообщение с сайта (#{@lang.upcase})")
   end
 end
